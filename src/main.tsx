178d688f1328c8fb99d8c2f005cb8ce3247433ae
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-10-19 23:00:59 +0300

    add: routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Categories from "./pages/Categories.tsx";
+const router = createBrowserRouter([
+  {
+    path: "/author-desserts/",
+    element: <App />,
+    children: [
+      {
+        path: "/author-desserts/",
+        element: <Home />,
+      },
+      {
+        path: "/author-desserts/categories",
+        element: <Categories />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
